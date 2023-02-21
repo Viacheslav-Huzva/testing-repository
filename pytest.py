ce9286fd2e7ee1f3f3e991b4df8c0bc622964476
DezSlav <huzvaviacheslav@gmail.com>
2023-02-21 23:55:19 +0200

    my first ramdomaze

---
+++ pytest.py
@@ -0,0 +1,8 @@
+print(5+9)
+print(9*6)
+print("your name")
+print("Hello World!")
+name = input ( 'What it your name?')
+print(f"Hello {name}")
+number = input ("guess a number from one to nine " )
+print (f"number{1-9}")
